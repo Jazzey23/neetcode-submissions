249955774c494de4223f0bf8c6673c3b9b9ffde5
Jazzey23 <145479778+Jazzey23@users.noreply.github.com>
2026-07-02 20:59:38 +0800

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> returnable;
+        unordered_map<int, int> frq;
+        unordered_map<int, vector<int>> kmap;
+
+        for(int i = 1; i <= nums.size(); i++){
+            kmap[i];
+        }
+        
+        for(int num : nums){
+            frq[num]++;
+        }
+
+
+        for(const auto& [key, value] : frq){
+            kmap[value].push_back(key);
+        }
+
+        for(int i = nums.size(); i > 0; i--){
+            for(int num : kmap[i]){
+                
+                returnable.push_back(num);
+                k--;
+
+                if(k == 0){
+                    break;
+                }
+
+            }
+
+            if(k == 0){
+                    break;
+            }
+
+
+        }
+
+        
+
+
+
+        return returnable;
+
+    }
+};
