0af9f73ef619d339220af2e991b9b728f08536a3
Jazzey23 <145479778+Jazzey23@users.noreply.github.com>
2026-07-08 18:17:05 +0800

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i = 0;
+        int j = 0;
+        int maxProfit = INT_MIN;
+        
+        while(j < prices.size()){
+            maxProfit = max(maxProfit, prices[j] - prices[i]);
+
+
+            while(prices[i] > prices[j]){
+               i = j;
+            }
+            j++;
+
+        }
+        
+        return maxProfit;
+        
+    }
+};
